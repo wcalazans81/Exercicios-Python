4586476c201d60629d9d5e722011369ab330a73f
Wellington Calazans <wcalazans81@gmail.com>
2022-01-14 23:51:11 -0300

    Exercícios até a aula 04

---
+++ ex002.py
@@ -0,0 +1,4 @@
+dia = input('Digite o dia do seu nascimento ')
+mes = input('Digite o mes do seu nascimento ')
+ano = input('Digite o ano do seu nascimento ')
+print('Você nasceu em ',dia,'/',mes,'/',ano,'certo?')
---
+++ ex003.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite um valor'))
+n2 = float(input('Digite outro valor'))
+s = n1 + n2
+print('A some entre', n1, '+', n2, 'é iqual a', s)
